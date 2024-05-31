36079f03ce03d3e4afb6ea61e50f2fc9e62102af
NinaHerlin <ninaherlinaa31@gmail.com>
2024-05-31 18:02:21 +0700

    Update main.java
    Edgw Class

--- main.java
+++ main.java
@@ -1,1 +1,16 @@
 import java.util.*
+public class Main {
+
+    static class Edge implements Comparable<Edge> {
+        int u, v, weight;
+
+        public Edge(int u, int v, int weight) {
+            this.u = u;
+            this.v = v;
+            this.weight = weight;
+        }
+
+        public int compareTo(Edge other) {
+            return this.weight - other.weight;
+        }
+    }
