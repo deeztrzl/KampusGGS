99d153f466ebd2d2fa520880adcc390083faa5e8
deeztrzl <148042421+deeztrzl@users.noreply.github.com>
2024-06-03 14:44:49 +0700

    add find and union method at main.java

--- main.java
+++ main.java
@@ -14,3 +14,28 @@ public class Main {
             return this.weight - other.weight;
         }
     }
+
+    static int find(int[] parent, int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent, parent[i]);
+        }
+        return parent[i];
+    }
+
+    static void union(int[] parent, int[] ranking, int x, int y) {
+        int rootX = find(parent, x);
+        int rootY = find(parent, y);
+
+        if (rootX != rootY) {
+            if (ranking[rootX] > ranking[rootY]) {
+                parent[rootY] = rootX;
+            } else if (ranking[rootX] < ranking[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                ranking[rootX]++;
+            }
+        }
+    }
+//new code here
+}
